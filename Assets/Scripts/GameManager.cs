1ed0bb0beaf052b73d2ad5fc35de5bd3d7360572
MisterShake2099 <mistershake2099@gmail.com>
2018-11-04 01:17:27 -0800

    Created OverviewScreen. Fixed Buttons, generally.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,9 +6,9 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
 	public static GameManager instance = null;
-	public string screenToLoad = "Title";
 	public GameObject titleScreen;
 	public GameObject introScreen;
+	public GameObject overviewScreen;
 
 
 	private void Start()
@@ -24,22 +24,7 @@ public class GameManager : MonoBehaviour
 		}
 
 		DontDestroyOnLoad(gameObject);
-	}
-
-	private void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
-	{
-		if (screenToLoad.Equals("Title"))
-		{
-			LoadTitleScreen();
-		}
-	}
-	private void OnEnable()
-	{
-		SceneManager.sceneLoaded += OnLevelFinishedLoading;
-	}
-	private void OnDisable()
-	{
-		SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+		LoadTitleScreen();
 	}
 
 	private void Update()
@@ -47,15 +32,16 @@ public class GameManager : MonoBehaviour
 
 	}
 
+
 	private void PreloadScreens()
 	{
 		titleScreen = Instantiate(titleScreen, new Vector3(0, 0, 0), Quaternion.identity);
 		introScreen = Instantiate(introScreen, new Vector3(0, 0, 0), Quaternion.identity);
+		overviewScreen = Instantiate(overviewScreen, new Vector3(0, 0, 0), Quaternion.identity);
 	}
 
 	private void LoadTitleScreen()
 	{
-		//if (titleScreen.)
 		titleScreen.SetActive(true);
 		titleScreen.GetComponentInChildren<Canvas>().GetComponentInChildren<Button>().onClick.AddListener(StartClick);
 	}
@@ -63,13 +49,26 @@ public class GameManager : MonoBehaviour
 	private void LoadIntroScreen()
 	{
 		introScreen.SetActive(true);
+		introScreen.GetComponentInChildren<Canvas>().GetComponentInChildren<Button>().onClick.AddListener(ContinueClick);
+
 	}
+
+	private void LoadOverviewScreen()
+	{
+		overviewScreen.SetActive(true);
+	}
+
 
 	private void StartClick()
 	{
-		Debug.Log("START CLICK");
 		titleScreen.SetActive(false);
 		LoadIntroScreen();
+	}
+
+	private void ContinueClick()
+	{
+		introScreen.SetActive(false);
+		LoadOverviewScreen();
 	}
 
 }
