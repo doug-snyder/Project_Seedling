337ae455613aed1d56b4b6660f393702b3f16c22
MisterShake2099 <mistershake2099@gmail.com>
2018-12-30 18:26:22 -0800

    Begin creating some dev tools.

---
+++ Assets/Scripts/DEV_NavButtons.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DEV_NavButtons : MonoBehaviour
+{
+	public static DEV_NavButtons instance = null;
+
+	private void Start()
+	{
+		if (instance == null)
+		{
+			instance = this;
+		}
+		else if (instance != null)
+		{
+			Destroy(gameObject);
+		}
+		DontDestroyOnLoad(gameObject);
+	}
+
+}
