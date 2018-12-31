143199257bb5ebdfc0c8fd47da8a2095a58c503e
MisterShake2099 <mistershake2099@gmail.com>
2018-12-30 18:18:52 -0800

    Save files before line-ending refresh.

--- Assets/Scripts/LoaderScene.cs
+++ Assets/Scripts/LoaderScene.cs
@@ -4,12 +4,14 @@
 public class LoaderScene : MonoBehaviour
 {
 	public GameObject gameManagerScene;
+	public DEV_NavButtons DEV_NavButtons;
 
 	void Awake()
 	{
 		if (GameManagerScene.instance == null)
 		{
 			Instantiate(gameManagerScene);
+			Instantiate(DEV_NavButtons);
 		}
 	}
 
