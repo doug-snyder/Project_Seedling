2adbd58076904b0c44b70600a4f4ec1403ddce6c
Doug <mistershake2099@gmail.com>
2019-01-20 01:23:22 -0800

    Clean up pointless scripts. Make inter-scent handlers for fields and input fields.

---
+++ Assets/Scripts/FieldHandler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FieldHandler : MonoBehaviour
+{
+	public string field;    
+
+    void Start()
+    {
+		Text text = gameObject.GetComponent<Text>();
+		text.text = PlayerPrefs.GetFloat(field).ToString();
+    }
+
+}
---
+++ Assets/Scripts/InputFieldHandler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InputFieldHandler : MonoBehaviour
+{
+	public string field;
+	InputField inputField;
+
+	private void Start()
+	{
+		inputField = gameObject.GetComponent<InputField>();
+
+		inputField.text = PlayerPrefs.GetFloat(field).ToString();
+	}
+
+	public void SaveFieldAsFloat()
+	{
+		PlayerPrefs.SetFloat(field, float.Parse(inputField.text));
+
+		Debug.Log(PlayerPrefs.GetFloat(field));
+	}
+
+}
