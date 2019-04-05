7f7e80c83254d31ab78cb4e02da866332e1b6bd1
Doug <mistershake2099@gmail.com>
2019-04-05 00:42:12 -0700

    Create TextUpdater for INotifyPropertyChanged implementation.

---
+++ Assets/Script/TextUpdater.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+
+namespace ColdPants.ProjectSeedling
+{
+	public class TextUpdater : INotifyPropertyChanged //MonoBehaviour
+	{
+
+		private string _fieldText;
+
+		public string FieldText
+		{
+			get { return _fieldText; }
+			private set
+			{
+				_fieldText = value;
+				OnPropertyChanged("FieldText");
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+
+		protected virtual void OnPropertyChanged(string propertyName)
+		{
+			PropertyChangedEventHandler handler = PropertyChanged;
+			if (handler != null)
+			{
+				handler(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+
+	}
+}
