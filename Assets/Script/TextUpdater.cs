de9da505e96dccd79e720e97eb769f69df3c9499
Doug <mistershake2099@gmail.com>
2019-04-10 10:19:08 -0700

    Decided to go with custom handlers for each page.

--- Assets/Script/TextUpdater.cs
+++ Assets/Script/TextUpdater.cs
@@ -2,30 +2,33 @@
 
 namespace ColdPants.ProjectSeedling
 {
-	public class TextUpdater : INotifyPropertyChanged //MonoBehaviour
+	public class TextUpdater : INotifyPropertyChanged
 	{
-
-		private string _fieldText;
-
-		public string FieldText
-		{
-			get { return _fieldText; }
-			private set
-			{
-				_fieldText = value;
-				OnPropertyChanged("FieldText");
-			}
-		}
+		private string _TaxRate;
 
 		public event PropertyChangedEventHandler PropertyChanged;
 
+		public string TaxRate
+		{
+			get { return _TaxRate; }
+			private set
+			{
+				_TaxRate = value;
+				OnPropertyChanged("TaxRate");
+			}
+		}
 
 		protected virtual void OnPropertyChanged(string propertyName)
+		{
+			OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+		}
+
+		protected virtual void OnPropertyChanged(PropertyChangedEventArgs args)
 		{
 			PropertyChangedEventHandler handler = PropertyChanged;
 			if (handler != null)
 			{
-				handler(this, new PropertyChangedEventArgs(propertyName));
+				handler(this, args);
 			}
 		}
 
